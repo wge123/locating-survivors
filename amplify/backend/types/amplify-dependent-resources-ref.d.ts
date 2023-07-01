59cca651ae621d9ab97a3ed213baddaf826f4fa1
Conroy Ricketts <conroy.ricketts@tutanota.com>
2023-07-01 15:56:38 -0400

    Set up backend.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,32 +1,32 @@
 export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "locatingsurvivors99e79b75": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
+  'auth': {
+    'locatingsurvivors99e79b75': {
+      'AppClientID': 'string',
+      'AppClientIDWeb': 'string',
+      'IdentityPoolId': 'string',
+      'IdentityPoolName': 'string',
+      'UserPoolArn': 'string',
+      'UserPoolId': 'string',
+      'UserPoolName': 'string'
     }
   },
-  "function": {
-    "usersfunction": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+  'function': {
+    'usersfunction': {
+      'Arn': 'string',
+      'LambdaExecutionRole': 'string',
+      'LambdaExecutionRoleArn': 'string',
+      'Name': 'string',
+      'Region': 'string'
     }
   },
-  "storage": {
-    "usersDb": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
+  'storage': {
+    'usersDb': {
+      'Arn': 'string',
+      'Name': 'string',
+      'PartitionKeyName': 'string',
+      'PartitionKeyType': 'string',
+      'Region': 'string',
+      'StreamArn': 'string'
     }
   }
 }
