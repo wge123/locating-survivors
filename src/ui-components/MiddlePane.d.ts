bd056702a62c367314e880c72da8b883f02817cc
wilhelm <willeurton@gmail.conm>
2023-08-27 20:31:29 -0400

    Auto Generated Cognito UI

---
+++ src/ui-components/MiddlePane.d.ts
@@ -0,0 +1,26 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { EscapeHatchProps } from "@aws-amplify/ui-react/internal";
+import { TextProps, ViewProps } from "@aws-amplify/ui-react";
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type MiddlePaneOverridesProps = {
+    MiddlePane?: PrimitiveOverrideProps<ViewProps>;
+    "Window Background"?: PrimitiveOverrideProps<ViewProps>;
+    "Forgot Password..."?: PrimitiveOverrideProps<TextProps>;
+    "Password Entry"?: PrimitiveOverrideProps<ViewProps>;
+    "Rectangle "?: PrimitiveOverrideProps<ViewProps>;
+    "Password..."?: PrimitiveOverrideProps<TextProps>;
+    "Username Entry"?: PrimitiveOverrideProps<ViewProps>;
+    Rectangle?: PrimitiveOverrideProps<ViewProps>;
+    "Username..."?: PrimitiveOverrideProps<TextProps>;
+    "Search and Rescue Cellular Forensics Service"?: PrimitiveOverrideProps<TextProps>;
+} & EscapeHatchProps;
+export declare type MiddlePaneProps = React.PropsWithChildren<Partial<ViewProps> & {
+    overrides?: MiddlePaneOverridesProps | undefined | null;
+}>;
+export default function MiddlePane(props: MiddlePaneProps): React.ReactElement;
