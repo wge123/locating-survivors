463ac68e947cedca9324857a132012a1957b8433
wilhelm <willeurton@gmail.conm>
2023-08-27 20:33:13 -0400

    updated Amplify folder

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,32 +1,21 @@
 export type AmplifyDependentResourcesAttributes = {
-  'auth': {
-    'locatingsurvivors99e79b75': {
-      'AppClientID': 'string',
-      'AppClientIDWeb': 'string',
-      'IdentityPoolId': 'string',
-      'IdentityPoolName': 'string',
-      'UserPoolArn': 'string',
-      'UserPoolId': 'string',
-      'UserPoolName': 'string'
+  "auth": {
+    "lsAuth": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
     }
   },
-  'function': {
-    'usersfunction': {
-      'Arn': 'string',
-      'LambdaExecutionRole': 'string',
-      'LambdaExecutionRoleArn': 'string',
-      'Name': 'string',
-      'Region': 'string'
-    }
-  },
-  'storage': {
-    'usersDb': {
-      'Arn': 'string',
-      'Name': 'string',
-      'PartitionKeyName': 'string',
-      'PartitionKeyType': 'string',
-      'Region': 'string',
-      'StreamArn': 'string'
+  "storage": {
+    "usersDb": {
+      "Arn": "string",
+      "Name": "string",
+      "PartitionKeyName": "string",
+      "PartitionKeyType": "string",
+      "Region": "string",
+      "StreamArn": "string"
     }
   }
 }
