2e80d551f1fbc55dee82187f5d9dbff23e130e05
Conroy Ricketts <conroy.ricketts@tutanota.com>
2023-09-02 20:34:56 -0400

    Refactor folder names to be snake case.
    Refactored 'lsAuth'.
    
    Refactored 'ui-components'.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,21 +1,21 @@
 export type AmplifyDependentResourcesAttributes = {
-  "auth": {
-    "lsAuth": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
+  'auth': {
+    'lsAuth': {
+      'AppClientID': 'string',
+      'AppClientIDWeb': 'string',
+      'UserPoolArn': 'string',
+      'UserPoolId': 'string',
+      'UserPoolName': 'string'
     }
   },
-  "storage": {
-    "usersDb": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
+  'storage': {
+    'usersDb': {
+      'Arn': 'string',
+      'Name': 'string',
+      'PartitionKeyName': 'string',
+      'PartitionKeyType': 'string',
+      'Region': 'string',
+      'StreamArn': 'string'
     }
   }
 }
