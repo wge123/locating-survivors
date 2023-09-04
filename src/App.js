872dde02eb30848b99af5be2a2a6ee8541b7458e
wilhelm <willeurton@gmail.conm>
2023-09-04 19:38:11 -0400

    deleted redundant folder, restored amplify ui in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Authenticator } from '@aws-amplify/ui-react'
 import { Amplify } from 'aws-amplify'
 import React from 'react'
 import '@aws-amplify/ui-react/styles.css'
-import config from './aws-exports'
+import config from './awsexports'
 import Login from './screens/login.tsx'
 
 Amplify.configure(config)
@@ -11,6 +11,13 @@ Amplify.configure(config)
 
 export default function App() {
     return (
-        <Login />
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>Hello {user.username}</h1>
+                    <button onClick={signOut}>Sign out</button>
+                </main>
+            )}
+        </Authenticator>
     )
 }
