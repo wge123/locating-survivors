e8e293be5ad3f937acab6d2105ff0960d2f31d62
wilhelm <willeurton@gmail.conm>
2023-09-15 16:55:00 -0400

    added custom logic

--- amplify/backend/function/postCase/src/index.js
+++ amplify/backend/function/postCase/src/index.js
@@ -21,26 +21,78 @@ const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb')
 const client = new DynamoDBClient({
     region: process.env.AWS_REGION,
 })
+// import uuid to get random id
+const { v4: uuidv4 } = require('uuid')
 
 const documentClient = DynamoDBDocument.from(client)
 exports.handler = async (event) => {
+    const random_id = uuidv4()
+    const date = new Date();
+    const createdAt = date.toISOString();
+    const lastChangedAt = date.getTime();
     let body = event
     let item = {}
-    // takes every item passed in the body and adds it to the item
+    
     Object.keys(body).forEach(key => {
         item[key] = body[key]
     })
+    item.id = random_id
+
 
     try {
+        // first we check if user_id was passed in body
+        if (!item.user_id) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'user_id is required'
+                })
+            }
+        }
+        // scan a user table and return all of the ids
+        const scan = await documentClient.scan({
+            TableName: process.env.STORAGE_USER_NAME,
+            ProjectionExpression: 'id'
+            
+        })
+        
+        console.log(scan)
+        
+        const user_ids = scan.Items.map(item => item.id)
+        console.log(user_ids)   
+        if (user_ids.includes(item.user_id)) {
+            console.log('user_id exists')
+        }  
+        else if (!user_ids.includes(item.user_id)) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'user_id does not exist'
+                })
+            }
+        }
+    
 
-        /* TODO:
-            add time created, auto generated id, check that user_id exists
-                if fields are missing, either return error or add default values
-        */
+         item = {
+            id: item.id,
+            user_id: item.user_id,
+            name: item.name,
+            createdAt,
+            updatedAt: createdAt,
+            __typename: 'Case',
+            _lastChangedAt: lastChangedAt,
+            _version: 1,
+        };
+
+    
+        
+
         const data = await documentClient.put({
             TableName: process.env.STORAGE_CASE_NAME,
-            Item: item
+            Item: item,
+            
         })
+        
         console.log(data)
         return {
             statusCode: 200,
