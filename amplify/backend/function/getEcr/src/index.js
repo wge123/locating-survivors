50c9c5adadcc2b84e572942ddad1dde586f3d82e
wilhelm <willeurton@gmail.conm>
2023-09-18 14:58:12 -0400

    changed names, and added simple logic to getECR

--- amplify/backend/function/getEcr/src/index.js
+++ amplify/backend/function/getEcr/src/index.js
@@ -13,20 +13,53 @@
 	STORAGE_USER_ARN
 	STORAGE_USER_NAME
 	STORAGE_USER_STREAMARN
+Amplify Params - DO NOT EDIT *//* Amplify Params - DO NOT EDIT
+    API_LSAPI_APIID
+    API_LSAPI_APINAME
+    AUTH_LSAUTH_USERPOOLID
+    ENV
+    REGION
+    STORAGE_CASE_ARN
+    STORAGE_CASE_NAME
+    STORAGE_CASE_STREAMARN
+    STORAGE_ECR_ARN
+    STORAGE_ECR_NAME
+    STORAGE_ECR_STREAMARN
+    STORAGE_USER_ARN
+    STORAGE_USER_NAME
+    STORAGE_USER_STREAMARN
 Amplify Params - DO NOT EDIT */
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-};
+    // lets make an empty try catch
+    // lets get id from queryString
+    const ecr_id = event.queryStringParameters.id
+
+    try {
+        // get item from dynamoDBTable ECR
+        const params = {
+            TableName: process.env.STORAGE_ECR_NAME,
+            Key: {
+                id: ecr_id,
+            }
+        }
+        const data = await documentClient.get(params)
+        return {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            statusCode: 200,
+            body: JSON.stringify(data.Item),
+        }
+
+
+
+
+    } catch (error) {
+        console.log(error)
+    }
+
+}
