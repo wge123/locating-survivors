0fef9c36b075adb2b6bebcdf9e5428ec9ef773ba
wilhelm <willeurton@gmail.conm>
2023-09-18 16:49:59 -0400

    custom logic created for both getECR and deleteECR

--- amplify/backend/function/deleteEcr/src/index.js
+++ amplify/backend/function/deleteEcr/src/index.js
@@ -1,29 +1,54 @@
 /* Amplify Params - DO NOT EDIT
-	ENV
-	REGION
-	STORAGE_CASE_ARN
-	STORAGE_CASE_NAME
-	STORAGE_CASE_STREAMARN
-	STORAGE_ECR_ARN
-	STORAGE_ECR_NAME
-	STORAGE_ECR_STREAMARN
-	STORAGE_USER_ARN
-	STORAGE_USER_NAME
-	STORAGE_USER_STREAMARN
+    API_LSAPI_APIID
+    API_LSAPI_APINAME
+    AUTH_LSAUTH_USERPOOLID
+    ENV
+    REGION
+    STORAGE_CASE_ARN
+    STORAGE_CASE_NAME
+    STORAGE_CASE_STREAMARN
+    STORAGE_ECR_ARN
+    STORAGE_ECR_NAME
+    STORAGE_ECR_STREAMARN
+    STORAGE_USER_ARN
+    STORAGE_USER_NAME
+    STORAGE_USER_STREAMARN
 Amplify Params - DO NOT EDIT */
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb')
+const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb')
+const client = new DynamoDBClient({
+    region: process.env.AWS_REGION,
+})
+const documentClient = DynamoDBDocument.from(client)
 exports.handler = async (event) => {
-    console.log(`EVENT: ${JSON.stringify(event)}`);
-    return {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  },
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-};
+    const ecr_id = event.queryStringParameters.id
+
+    try {
+
+        const params = {
+            TableName: process.env.STORAGE_ECR_NAME,
+            Key: {
+                id: ecr_id,
+            }
+        }
+
+        const data = await documentClient.delete(params)
+        return {
+            statusCode: 200,
+            body: JSON.stringify('ECR deleted')
+        }
+    } catch (err) {
+        console.error('Error deleting item:', err)
+        return {
+            statusCode: 500,
+            body: JSON.stringify('Error deleting item')
+        }
+    }
+}
+
+
+
--- amplify/backend/function/getEcr/src/index.js
+++ amplify/backend/function/getEcr/src/index.js
@@ -1,19 +1,4 @@
 /* Amplify Params - DO NOT EDIT
-	API_LSAPI_APIID
-	API_LSAPI_APINAME
-	AUTH_LSAUTH_USERPOOLID
-	ENV
-	REGION
-	STORAGE_CASE_ARN
-	STORAGE_CASE_NAME
-	STORAGE_CASE_STREAMARN
-	STORAGE_ECR_ARN
-	STORAGE_ECR_NAME
-	STORAGE_ECR_STREAMARN
-	STORAGE_USER_ARN
-	STORAGE_USER_NAME
-	STORAGE_USER_STREAMARN
-Amplify Params - DO NOT EDIT *//* Amplify Params - DO NOT EDIT
     API_LSAPI_APIID
     API_LSAPI_APINAME
     AUTH_LSAUTH_USERPOOLID
@@ -33,13 +18,17 @@ Amplify Params - DO NOT EDIT */
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb')
+const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb')
+const client = new DynamoDBClient({
+    region: process.env.AWS_REGION,
+})
+const documentClient = DynamoDBDocument.from(client)
 exports.handler = async (event) => {
-    // lets make an empty try catch
-    // lets get id from queryString
     const ecr_id = event.queryStringParameters.id
 
     try {
-        // get item from dynamoDBTable ECR
+
         const params = {
             TableName: process.env.STORAGE_ECR_NAME,
             Key: {
@@ -54,9 +43,6 @@ exports.handler = async (event) => {
             statusCode: 200,
             body: JSON.stringify(data.Item),
         }
-
-
-
 
     } catch (error) {
         console.log(error)
