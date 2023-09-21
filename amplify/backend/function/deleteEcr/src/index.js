57489a119a0f5e9f242e565c11bf9c5be720c739
wilhelm <willeurton@gmail.conm>
2023-09-21 17:55:58 -0400

    fixed overwritten get and delete ECR

--- amplify/backend/function/deleteEcr/src/index.js
+++ amplify/backend/function/deleteEcr/src/index.js
@@ -4,20 +4,15 @@
     AUTH_LSAUTH_USERPOOLID
     ENV
     REGION
-    STORAGE_CASE_ARN
-    STORAGE_CASE_NAME
-    STORAGE_CASE_STREAMARN
     STORAGE_ECR_ARN
     STORAGE_ECR_NAME
     STORAGE_ECR_STREAMARN
-    STORAGE_USER_ARN
-    STORAGE_USER_NAME
-    STORAGE_USER_STREAMARN
 Amplify Params - DO NOT EDIT */
 
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb')
 const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb')
 const client = new DynamoDBClient({
@@ -49,6 +44,4 @@ exports.handler = async (event) => {
         }
     }
 }
-
-
 
