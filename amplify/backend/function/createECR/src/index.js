3effe3c57bc5cbdf82dbcfa10229bdf7612136d0
wilhelm <willeurton@gmail.conm>
2023-09-22 14:57:50 -0400

    fixed createECR function

--- amplify/backend/function/createECR/src/index.js
+++ amplify/backend/function/createECR/src/index.js
@@ -17,166 +17,115 @@ Amplify Params - DO NOT EDIT */
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
+// database imports and clients
 const { DynamoDBClient } = require('@aws-sdk/client-dynamodb')
 const { DynamoDBDocument } = require('@aws-sdk/lib-dynamodb')
+
 const client = new DynamoDBClient({
     region: process.env.AWS_REGION,
 })
+const documentClient = DynamoDBDocument.from(client)
 // import uuid to get random id
 const { v4: uuidv4 } = require('uuid')
+// Constants
+const CORS_HEADERS = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+    'Access-Control-Allow-Headers': '*'
+}
+const TABLE_NAME = process.env.STORAGE_ECR_NAME
 
-const documentClient = DynamoDBDocument.from(client)
+
+// Required fields for Posting ECR
+const requiredFields = [
+    'name',
+    'phone_number',
+    'phone_provider',
+    'case_id',
+    'email',
+    'subscriber_information',
+    'periodic_location_updates',
+    'last_known_information',
+    'duration',
+    'call_detail_no_sites',
+    'call_detail_with_sites',
+    'sms_detail'
+]
+
+
+
 exports.handler = async (event) => {
 
-    const ecr_id = uuidv4()
-    // const date = new Date()
-    // const createdAt = date.toISOString()
-    // const lastChangedAt = date.getTime()
-    let body = event
-    let item = {}
 
-    Object.keys(body).forEach(key => {
-        item[key] = body[key]
-    })
 
     try {
 
+        let item = getItemFromBody(event)
+        // validate required fields
+        validateFields(item, requiredFields)
+        // initialize some fields that need to be included in post
+
+        // add the fields to the item, may optimize this later
+        const ecr_id = uuidv4()
+        const date = new Date().toISOString()
+        const updated_at = new Date().toDateString()
+        const created_at = new Date().toGMTString()
+        const version = 1
+        item.date = date
         item.id = ecr_id
+        item.updatedAt = updated_at
+        item.createdAt = created_at
+        item.version = version
 
-        // check to see if case_id was passed in body, only required paramater
-        if (!item.case_id) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: 'case_id is required'
-                })
-            }
-        }
-
-        // lets query the case table to see if the case_id exists
-        const case_query = await documentClient.query({
-            TableName: process.env.STORAGE_CASE_NAME,
-            KeyConditionExpression: 'id = :case_id',
-            ExpressionAttributeValues: {
-                ':case_id': item.case_id
-            }
-        })
-        // check if const query is null
-
-        if (case_query.Count === 0 || !case_query) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: 'case_id does not exist'
-                })
-            }
-        }
-
-        // retrieve user_id from case table
-        const user_id = case_query.Items[0].user_id
-        if (user_id) item.user_id = user_id
-        else {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: 'user_id does not exist in case table'
-                })
-            }
-        }
-
-        const case_table_id = case_query.Items[0].id
-        if (case_table_id) item.case_id = case_table_id
-        const case_table_cell_number = case_query.Items[0].cell_number
-        if (case_table_cell_number) item.cell_number = case_table_cell_number
-
-        if (case_query.Items[0].cell_provider) item.cell_provider = case_query.Items[0].cell_provider
+        await postItemToDatabase(item)
 
 
-
-        // lets query user table to see if user_id exists
-        const user_query = await documentClient.query({
-            TableName: process.env.STORAGE_USER_NAME,
-            KeyConditionExpression: 'id = :user_id',
-            ExpressionAttributeValues: {
-                ':user_id': user_id
-            }
-        })
-        // check if const query is null
-        if (user_query.Count === 0 || !user_query) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: 'user_id does not exist'
-                })
-            }
-        }
-
-        // get name and email from user table
-        const user_name = user_query.Items[0].name
-        const email = user_query.Items[0].email
+        return apiResponse(200, { message: 'success' });
 
 
-        /* We will be checking to see if the expected fields are passed into the request body
-            if they are not they will be assigned a default value or return an error
-         */
-
-        // this should never happen
-        if (!item.id) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: 'ID should exist'
-                })
-            }
-        }
-
-        if (!item.case_id) {
-            return {
-                statusCode: 400,
-                body: JSON.stringify({
-                    message: 'case_id should exist'
-                })
-            }
-        }
-
-
-        //Here we are going to assign default values to the fields that are not passed in the request body
-
-
-        if (!item.cell_number) item.cell_number = case_table_cell_number
-        if (!item.cell_provider) item.cell_provider = 'Sprint'
-        if (!item.name) item.name = user_name
-        if (!item.email) item.email = email
-        if (!item.date) item.date = new Date().toISOString() // need to add date
-        if (!item.subscriber_info) item.subscriber_info = false
-        if (!item.periodic_location) item.periodic_location = false
-        if (!item.last_known_information) item.last_known_information = false
-        if (!item.duration) item.duration = 0
-        if (!item.call_detail) item.call_detail = false
-        if (!item.sms_detail) item.sms_detail = false
-
-        // End of expected fields
-
-
-
-        // now we put the item in the table!
-
-        const put_item = await documentClient.put({
-            TableName: process.env.STORAGE_ECR_NAME,
-            Item: item
-
-        })
-
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify('ECR created', put_item)
-        }
 
     } catch (error) {
-        console.log(error)
-        return error
+
+        return apiResponse(400, { message: error.message });
     }
+}
+
+
+
+// Lets make some helper functions
+function getItemFromBody(body) {
+    let item = {}
+    Object.keys(body).forEach(key => {
+        item[key] = body[key]
+    })
+    return item
+
+}
+function validateFields(item, fields) {
+    for (const field of fields) {
+        if (!item.hasOwnProperty(field)) {
+            throw new Error(`Missing field: ${field}`)
+        }
+    }
+}
+async function postItemToDatabase(item) {
+    const params = {
+        TableName: TABLE_NAME,
+        Item: item
+    }
+    await documentClient.put(params)
+}
+
+
+function apiResponse(statusCode, body) {
+    return {
+        statusCode,
+        headers: CORS_HEADERS,
+        body: JSON.stringify(body)
+    };
 
 
 }
+
+
+
