7828f3026a622caee9d9069974ba51eb3e7ede1a
Conroy Ricketts <conroy.ricketts@tutanota.com>
2023-10-05 19:45:57 -0400

    Added logic to App.js to listen for user auth events, and conditionally render the login screen.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
-import { Authenticator } from '@aws-amplify/ui-react'
-import { Amplify } from 'aws-amplify'
-import React from 'react'
 import '@aws-amplify/ui-react/styles.css'
-import config from './aws-exports'
-import NewCaseScreen from './screens/new_case_screen/newCase'
+import { Amplify, Hub } from 'aws-amplify'
+import React, { useEffect, useState } from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import awsExports from './aws-exports'
+import ECRBuilderScreen from './screens/ECR_builder_screen/ecrBuilder'
 import CaseListScreen from './screens/case_list_screen/caseList'
 import CaseViewScreen from './screens/case_view_screen/caseView'
-import ECRBuilderScreen from './screens/ECR_builder_screen/ecrBuilder'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import LoginScreen from './screens/login_screen/login'
+import NewCaseScreen from './screens/new_case_screen/newCase'
 
-Amplify.configure(config)
+Amplify.configure(awsExports)
 
 // If we add admin panel we need more routes here, probably sub routes
 // ecrbuilder route might need to be private and only receive user context from componenets through navigation but idk yet
 // tbqh we should use React context to grab the user info but this is ok for now
 export default function App() {
-    return(
-        <Authenticator>
-            {({  user }) => (
-                <Router>
-                    <Routes>
-                        <Route path="*" element={<CaseListScreen user={user}/>} />
-                        <Route path="/new_case" element={<NewCaseScreen user={user} />}/>
-                        <Route path="/case_view" element={<CaseViewScreen user={user}/>}/>
-                        <Route path="/ecr_builder" element={<ECRBuilderScreen user={user}/>}/>
-                        <Route path="/case_list" element={<CaseListScreen user={user}/>} />
-                    </Routes>
-                </Router>
-            )}
-        </Authenticator>
+    const [currentUser, setCurrentUser] = useState(null)
+
+    useEffect(() => {
+        Hub.listen('auth', (event) => {
+            console.log('auth event', event)
+            const cognitoUser = event.payload.data
+            setCurrentUser(cognitoUser)
+        })
+    })
+
+    return (
+        currentUser != null ?
+            <Router>
+                <Routes>
+                    <Route path="*" element={<CaseListScreen user={currentUser}/>} />
+                    <Route path="/new_case" element={<NewCaseScreen user={currentUser} />}/>
+                    <Route path="/case_view" element={<CaseViewScreen user={currentUser}/>}/>
+                    <Route path="/ecr_builder" element={<ECRBuilderScreen user={currentUser}/>}/>
+                    <Route path="/case_list" element={<CaseListScreen user={currentUser}/>} />
+                </Routes>
+            </Router> :
+            <LoginScreen />
     )
 }
