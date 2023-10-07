005d1f5b304dd6807268a72d65e8d312227b822a
akeenl-sp <akeen.lewis@smartrent.com>
2023-10-07 17:38:23 -0400

    case list view done & csv export done

--- src/screens/case_list_screen/caseList.tsx
+++ src/screens/case_list_screen/caseList.tsx
@@ -1,6 +1,8 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import './caseList.css'
-
+import PropTypes from 'prop-types'
+import moment from 'moment-timezone'
+import {useLocation, useNavigate} from 'react-router-dom'
 // This is NOT a model for an actual case from the backend. 
 // This is simply the information we need for displaying the case on the frontend. 
 // We should fill these parameters with data that we GET from the backend.
@@ -10,8 +12,107 @@ interface CaseForDisplay {
     duration: string
 }
 
-export default function CaseListScreen(): JSX.Element {
-    const [cases, setCases] = useState<CaseForDisplay[]>([])
+export default function CaseListScreen(props): JSX.Element {
+    const [cases, setCases] = useState([])
+    const [case_data_arr, set_case_data] = useState([])
+    const navigate = useNavigate()
+    CaseListScreen.propTypes = {
+        user: PropTypes.object.isRequired
+    }
+
+    const {user} = props
+
+    useEffect(() => {
+        // Assuming getCases is the function you defined to fetch data
+        const fetchData = async () => {
+            const fetchedCases = await getCases()
+            setCases(fetchedCases[0])
+            set_case_data(fetchedCases[1])
+        }
+
+        fetchData()
+    }, [])
+
+    // TODO: We need to modify this function to retrieve cases from the backend, and then fit that data to our "CaseForDisplay" UI model so that we may display those cases on the frontend.
+    // NOTE: The UI has already been tested with mock data using the "CaseForDisplay" model.
+    async function getCases(): Promise<(CaseForDisplay[] | any[])[]> {
+        const userID = user.attributes.sub
+        const url = `https://6u7yn5reri.execute-api.us-east-1.amazonaws.com/prod/case?user_id=${userID}`
+        const case_arr: CaseForDisplay[] = []
+        const case_data_arr = []
+        try{
+            const response = await fetch(url)
+            const data = await response.json()
+            data.data.forEach(function(user_case: any) {
+                const display_case: CaseForDisplay = {
+                    lastUpdate: moment(user_case.time_updated).tz('America/New_York').format('lll'),
+                    status: user_case.status? user_case.status : 'Open',
+                    duration: user_case.duration
+                }
+                case_arr.push(display_case)
+                case_data_arr.push(user_case)
+            })
+        }catch (error) {
+            console.log('Error: ',error)
+        }
+
+        return [case_arr ,case_data_arr]
+    }
+
+    function getUserFullName(): string {
+        return user.attributes.name
+    }
+
+    function navigateToViewCaseScreen(key) {
+        navigate('/case_detail  ')
+    }
+
+    function getCaseItem(key: number, lastUpdate: string, status: string, duration: string): JSX.Element {
+        return (
+            <div id='cl-ci-container' key={key}>
+                <div id='cl-ci-left-pane'>
+                    <p id='cl-cit-left-align' className='cl-ci-text'>{`Last Update: ${lastUpdate}`}</p>
+                    <p id='cl-cit-left-align' className='cl-ci-text'>{`Status: ${status}`}</p>
+                </div>
+                <div id='cl-ci-middle-pane'>
+                    <p id='cl-cit-center-align' className='cl-ci-text'>{`Duration: ${duration}`}</p>
+                </div>
+                <div id='cl-ci-right-pane'>
+                    <button id='cl-ci-top-button' onClick={() => exportCase(key)}>Export...</button>
+                    <button id='cl-ci-bottom-button' onClick={() => navigateToViewCaseScreen(key)}>View...</button>
+                </div>
+            </div>
+        )
+    }
+
+    function navigateToNewCaseScreen() {
+        navigate('/new_case')
+    }
+
+    function exportCase(index) {
+        // Generate CSV header
+        const header = Object.keys(case_data_arr[index]).join(',')
+
+        const row = Object.values(case_data_arr[index]).join(',')
+
+        const caseId = case_data_arr[index].id
+
+        // Combine header and row
+        const csvContent = header + '\n' + row
+
+        // Create a Blob with the CSV content
+        const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' })
+
+        // Create and click a hidden link to trigger the download
+        const link = document.createElement('a')
+        const url = URL.createObjectURL(blob)
+        link.setAttribute('href', url)
+        link.setAttribute('download', `case_${caseId}.csv`)
+        link.style.visibility = 'hidden'
+        document.body.appendChild(link)
+        link.click()
+        document.body.removeChild(link)
+    }
 
     return (
         <div id='cl-container'>
@@ -37,24 +138,6 @@ export default function CaseListScreen(): JSX.Element {
     )
 }
 
-function getCaseItem(key: number, lastUpdate: string, status: string, duration: string): JSX.Element {
-    return (
-        <div id='cl-ci-container' key={key}>
-            <div id='cl-ci-left-pane'>
-                <p id='cl-cit-left-align' className='cl-ci-text'>{`Last Update: ${lastUpdate}`}</p>
-                <p id='cl-cit-left-align' className='cl-ci-text'>{`Status: ${status}`}</p>
-            </div>
-            <div id='cl-ci-middle-pane'>
-                <p id='cl-cit-center-align' className='cl-ci-text'>{`Duration: ${duration}`}</p>
-            </div>
-            <div id='cl-ci-right-pane'>
-                <button id='cl-ci-top-button' onClick={() => exportCase()}>Export...</button>
-                <button id='cl-ci-bottom-button' onClick={() => navigateToViewCaseScreen()}>View...</button>
-            </div>
-        </div>
-    )
-}
-
 function getRadioButtonWithText(text: string): JSX.Element {
     return (
         <div id='cl-radio-button-with-text'>
@@ -64,34 +147,8 @@ function getRadioButtonWithText(text: string): JSX.Element {
     )
 }
 
-function exportCase() {
-    // TODO: We need to save the case info as a CSV. Should this be handled by the backend?
-    return
-}
-
-function navigateToViewCaseScreen() {
-    // TODO: We need to navigate to the case view screen.
-    return
-}
-
-function navigateToNewCaseScreen() {
-    // TODO: We need to navigate to the new case screen.
-    return
-}
-
-function getUserFullName(): string {
-    // TODO: We need to get this value from the backend.
-    return 'Edward X. Ample'
-}
-
 function getUserRole(): string {
     // "Operator" is the default role.
     // TODO: We need to get this value from the backend.
     return 'Operator'
-}
-
-// TODO: We need to modify this function to retrieve cases from the backend, and then fit that data to our "CaseForDisplay" UI model so that we may display those cases on the frontend.
-// NOTE: The UI has already been tested with mock data using the "CaseForDisplay" model.
-function getCases(): CaseForDisplay[] {
-    return []
 }
