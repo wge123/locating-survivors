df207627ae7b7e8693fb3aa8529a322c7a15365a
Conroy Ricketts <conroy.ricketts@tutanota.com>
2023-10-14 12:41:11 -0400

    Adding backend stuff that came from amplify I think.

---
+++ amplify/backend/function/emailListener/src/index.py
@@ -0,0 +1,75 @@
+import json
+import time
+import boto3
+import imaplib
+import email
+import time
+import os
+
+lambda_client = boto3.client('lambda')
+
+
+
+
+def listen_and_scrape_email(case_id):
+        minutes = 5 * 60
+        sleep_seconds = 2
+        email_handle = "locatingsurvivorsemailtest"
+        
+        # Log in to your Gmail account and navigate to the inbox
+        mail = imaplib.IMAP4_SSL('imap.gmail.com')
+        mail.login('locatingsurvivorsemailtest@gmail.com', 'vgfz ecvi epll kzzw')
+
+
+        start_time = time.time()
+        case_address = f"{email_handle}+{case_id}@gmail.com"
+
+        
+        email_counter = 0
+        ## infinite loop to listen and scrape email
+        while True:
+            # Check if alloted listening time has passed
+            if time.time() - start_time > (minutes):
+                break
+
+            
+            # Check for new incoming messages only to specific case address
+            mail.select('Inbox')
+            result, data = mail.search(None, 'UNSEEN', 'TO', case_address)
+            
+            # checks if mail search worked
+            if result == 'OK':
+                # loops over data returned from search
+                for num in data[0].split():
+                    # Retrieve the email by sequential ID, this includes all the metadata
+                    _, msg_data = mail.fetch(num, '(RFC822)')
+                    # extracts just the email portion
+                    raw_email = msg_data[0][1]
+                    email_message = email.message_from_bytes(raw_email)
+                    # still needs to check if the message is going to specific address
+                    if case_address in email_message['To']:
+                        ## goes through message to extract only the body
+                        for part in email_message.walk():
+                            if part.get_content_type() == "text/plain":
+                                body = part.get_payload(decode=True)
+                                body = body.decode()
+                                break
+                        email_counter+=1        
+                        print(f"EMAIL # {email_counter} FOUND") 
+                        # invoke emailScraper
+                        lambda_payload = json.dumps(body)
+                        lambda_client.invoke(FunctionName=os.environ['FUNCTION_EMAILSCRAPER_NAME'], 
+                     InvocationType='Event',
+                     Payload=lambda_payload)
+                        
+                    
+            # keeps email listener from timing out
+            mail.noop()
+            time.sleep(sleep_seconds)
+
+        
+
+def handler(event, context):
+    case_id = event.get("case_id")
+    listen_and_scrape_email(case_id)
+    
---
+++ amplify/backend/function/handler/src/index.js
@@ -0,0 +1,79 @@
+/* Amplify Params - DO NOT EDIT
+    ENV
+    FUNCTION_EMAILLISTENER_NAME
+    FUNCTION_EMAILSCRAPER_NAME
+    FUNCTION_EMAILSENDER_NAME
+    REGION
+Amplify Params - DO NOT EDIT */
+
+
+
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+
+// Load in Lambda Client
+const { LambdaClient, InvokeCommand } = require('@aws-sdk/client-lambda')
+const lambda = new LambdaClient({ region: process.env.AWS_REGION })
+
+exports.handler = async (event) => {
+
+    const case_id = event.case_id
+    const name = event.name
+    const phone_number = event.phone_number
+    let duration = event.duration
+    if (duration == null || duration == undefined) {
+        duration = 0
+    }
+    duration = parseInt(duration)
+
+    // event that we will be passing into sender
+    const senderPayload = {
+        case_id: case_id,
+        name: name,
+        phone_number: phone_number,
+        duration: duration
+    }
+
+    // event that we will be passing into listener
+    const listenerPayload = {
+        case_id: case_id
+
+    }
+
+    // paramaters for sender
+    const sender = {
+        FunctionName: process.env.FUNCTION_EMAILSENDER_NAME,
+        Payload: JSON.stringify(senderPayload),
+
+    }
+
+    // paramaters for listener
+    const listener = {
+        FunctionName: process.env.FUNCTION_EMAILLISTENER_NAME,
+        Payload: JSON.stringify(listenerPayload),
+
+    }
+
+    // invocation commands
+    const send_command = new InvokeCommand(sender)
+    const listen_command = new InvokeCommand(listener)
+
+
+    try {
+
+        await Promise.all([
+            lambda.send(listen_command),
+            lambda.send(send_command),
+        ])
+
+
+
+
+    } catch (err) {
+        console.error(err)
+    }
+
+}
+
