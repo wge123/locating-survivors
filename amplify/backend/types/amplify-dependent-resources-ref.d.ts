d1fd646de65805c734370fadd01ce2741b428bb8
Conroy Ricketts <conroy.ricketts@tutanota.com>
2023-10-14 12:48:55 -0400

    Update login screen CSS to fix gap between components.

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,112 +1,112 @@
 export type AmplifyDependentResourcesAttributes = {
-  "api": {
-    "lsApi": {
-      "ApiId": "string",
-      "ApiName": "string",
-      "RootUrl": "string"
+  'api': {
+    'lsApi': {
+      'ApiId': 'string',
+      'ApiName': 'string',
+      'RootUrl': 'string'
     }
   },
-  "auth": {
-    "lsAuth": {
-      "AppClientID": "string",
-      "AppClientIDWeb": "string",
-      "IdentityPoolId": "string",
-      "IdentityPoolName": "string",
-      "UserPoolArn": "string",
-      "UserPoolId": "string",
-      "UserPoolName": "string"
+  'auth': {
+    'lsAuth': {
+      'AppClientID': 'string',
+      'AppClientIDWeb': 'string',
+      'IdentityPoolId': 'string',
+      'IdentityPoolName': 'string',
+      'UserPoolArn': 'string',
+      'UserPoolId': 'string',
+      'UserPoolName': 'string'
     }
   },
-  "function": {
-    "basicFunction": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+  'function': {
+    'basicFunction': {
+      'Arn': 'string',
+      'LambdaExecutionRole': 'string',
+      'LambdaExecutionRoleArn': 'string',
+      'Name': 'string',
+      'Region': 'string'
     },
-    "deleteCase": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+    'deleteCase': {
+      'Arn': 'string',
+      'LambdaExecutionRole': 'string',
+      'LambdaExecutionRoleArn': 'string',
+      'Name': 'string',
+      'Region': 'string'
     },
-    "deleteECR": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+    'deleteECR': {
+      'Arn': 'string',
+      'LambdaExecutionRole': 'string',
+      'LambdaExecutionRoleArn': 'string',
+      'Name': 'string',
+      'Region': 'string'
     },
-    "getCase": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+    'getCase': {
+      'Arn': 'string',
+      'LambdaExecutionRole': 'string',
+      'LambdaExecutionRoleArn': 'string',
+      'Name': 'string',
+      'Region': 'string'
     },
-    "getECR": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+    'getECR': {
+      'Arn': 'string',
+      'LambdaExecutionRole': 'string',
+      'LambdaExecutionRoleArn': 'string',
+      'Name': 'string',
+      'Region': 'string'
     },
-    "listCases": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+    'listCases': {
+      'Arn': 'string',
+      'LambdaExecutionRole': 'string',
+      'LambdaExecutionRoleArn': 'string',
+      'Name': 'string',
+      'Region': 'string'
     },
-    "postCase": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+    'postCase': {
+      'Arn': 'string',
+      'LambdaExecutionRole': 'string',
+      'LambdaExecutionRoleArn': 'string',
+      'Name': 'string',
+      'Region': 'string'
     },
-    "postECR": {
-      "Arn": "string",
-      "LambdaExecutionRole": "string",
-      "LambdaExecutionRoleArn": "string",
-      "Name": "string",
-      "Region": "string"
+    'postECR': {
+      'Arn': 'string',
+      'LambdaExecutionRole': 'string',
+      'LambdaExecutionRoleArn': 'string',
+      'Name': 'string',
+      'Region': 'string'
     }
   },
-  "storage": {
-    "Case": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
+  'storage': {
+    'Case': {
+      'Arn': 'string',
+      'Name': 'string',
+      'PartitionKeyName': 'string',
+      'PartitionKeyType': 'string',
+      'Region': 'string',
+      'StreamArn': 'string'
     },
-    "ECR": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
+    'ECR': {
+      'Arn': 'string',
+      'Name': 'string',
+      'PartitionKeyName': 'string',
+      'PartitionKeyType': 'string',
+      'Region': 'string',
+      'StreamArn': 'string'
     },
-    "User": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
+    'User': {
+      'Arn': 'string',
+      'Name': 'string',
+      'PartitionKeyName': 'string',
+      'PartitionKeyType': 'string',
+      'Region': 'string',
+      'StreamArn': 'string'
     },
-    "usersDb": {
-      "Arn": "string",
-      "Name": "string",
-      "PartitionKeyName": "string",
-      "PartitionKeyType": "string",
-      "Region": "string",
-      "StreamArn": "string"
+    'usersDb': {
+      'Arn': 'string',
+      'Name': 'string',
+      'PartitionKeyName': 'string',
+      'PartitionKeyType': 'string',
+      'Region': 'string',
+      'StreamArn': 'string'
     }
   }
 }
