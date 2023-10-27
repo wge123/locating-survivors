ac0009f7dd7d9668645d120f619445792d26aa71
Akeen <akeenlewis1@gmail.com>
2023-10-27 12:26:32 -0400

    adding mapbox api key request and error handeling

--- src/utils/map.tsx
+++ src/utils/map.tsx
@@ -2,68 +2,79 @@ import React, {useEffect, useState} from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import PropTypes from 'prop-types'
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faExclamationTriangle} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faExclamationTriangle} from '@fortawesome/free-solid-svg-icons'
 import '../screens/case_view_screen/caseView.css'
 
 // Mapbox token
 // add api call here
 
 export default function Map({ lat, lng }) {
-    const [mapboxToken, setMapboxToken] = useState<string>('');
-    const [hasError, setHasError] = useState<boolean>(false);
+    const [mapboxToken, setMapboxToken] = useState<string>('')
+    const [hasError, setHasError] = useState<boolean>(false)
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         const fetchToken = async () => {
             try {
-                const response = await fetch('/your-server-endpoint-for-token');
-                const data = await response.json();
-                if (data.token) {
-                    setMapboxToken(data.token);
+                const url = 'https://6u7yn5reri.execute-api.us-east-1.amazonaws.com/prod/keys/mapbox'
+                const response = await fetch(url)
+                const data = await response.json()
+                const token = JSON.parse(data.body).token
+                if (token) {
+                    setMapboxToken(token)
                 } else {
-                    setHasError(true);
+                    setHasError(true)
                 }
             } catch (error) {
-                setHasError(true);
-                console.error('Error fetching token:', error);
+                setHasError(true)
+                console.error('Error fetching token:', error)
             }
-        };
-        fetchToken();
-    }, []);
+            setIsLoading(false)
+        }
+        fetchToken()
+    }, [])
 
     useEffect(() => {
-        // test 1
-        if (!mapboxToken || !lat || !lng || hasError) {
-            return;
+
+        if (isLoading) return
+
+        if (!mapboxToken || !lat || !lng ) {
+            setHasError(true)
+            return
         }
 
-        mapboxgl.accessToken = mapboxToken;
+        mapboxgl.accessToken = mapboxToken
+        try {
+            const map = new mapboxgl.Map({
+                container: 'map',
+                style: 'mapbox://styles/mapbox/streets-v11',
+                center: [lng, lat],
+                zoom: 9,
+            })
 
-        const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [lng, lat],
-            zoom: 9,
-        });
+            new mapboxgl.Marker()
+                .setLngLat([lng, lat])
+                .addTo(map)
+        } catch (e) {
+            console.log(e)
+            setHasError(true)
+        }
 
-        new mapboxgl.Marker()
-            .setLngLat([lng, lat])
-            .addTo(map);
-    }, [lat, lng, mapboxToken, hasError]);
+    }, [lat, lng, mapboxToken, hasError, isLoading])
 
-    // test 2 and 3
     return hasError ? (
         <div id='cv-empty-map'>
             <FontAwesomeIcon icon={faExclamationTriangle} size="10x" id="faExclamationTriangle"/>
             <h2 id="errorHeader">Oops! Something Went Wrong.</h2>
-            <p>MapBox Didn't Load Correctly. Please Reach Out To Your IT Admin For Details</p>
+            <p>MapBox Didn&apos;t Load Correctly. Please Reach Out To Your IT Admin For Details</p>
         </div>
     ) : (
         <div id='map' style={{ height: '400px', width: '100%' }}></div>
-    );
+    )
 }
 
 Map.propTypes = {
     lat: PropTypes.number.isRequired,
     lng: PropTypes.number.isRequired,
-};
+}
