544f7939a41503bf8b5bcda16daf846f9857ca15
Lily Rubio <lilyrubio@joshs-air.attlocal.net>
2023-11-08 18:14:36 -0500

    Fixes #89

--- src/ui-components/LoginButton.d.ts
+++ src/ui-components/LoginButton.d.ts
@@ -6,22 +6,12 @@
 
 import * as React from "react";
 import { TextProps, ViewProps } from "@aws-amplify/ui-react";
-export declare type EscapeHatchProps = {
-    [elementHierarchy: string]: Record<string, unknown>;
-} | null;
-export declare type VariantValues = {
-    [key: string]: string;
-};
-export declare type Variant = {
-    variantValues: VariantValues;
-    overrides: EscapeHatchProps;
-};
 export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
 export declare type LoginButtonOverridesProps = {
     LoginButton?: PrimitiveOverrideProps<ViewProps>;
     Rectangle?: PrimitiveOverrideProps<ViewProps>;
     Login?: PrimitiveOverrideProps<TextProps>;
-} & EscapeHatchProps;
+};
 export declare type LoginButtonProps = React.PropsWithChildren<Partial<ViewProps> & {
     overrides?: LoginButtonOverridesProps | undefined | null;
 }>;
